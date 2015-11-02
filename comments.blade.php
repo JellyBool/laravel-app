d461b50cb57366e176347bdced8528482eeffc53
JellyBool <ly913417651@gmail.com>
2015-11-02 18:41:58 +0800

    init project

---
+++ comments.blade.php
@@ -0,0 +1,6 @@
+<div class="media-conversation-meta">
+<span class="media-conversation-replies">
+<a href="/discussion/154#reply">{{ count($discussion->comments) }}</a>
+回复
+</span>
+</div>
---
+++ dropdown.blade.php
@@ -0,0 +1,7 @@
+<ul class="dropdown-menu" aria-labelledby="dLabel">
+    <li><a href="#"> <i class="fa fa-user"></i> 更换头像</a></li>
+    <li><a href="#"> <i class="fa fa-cog"></i> 更换密码</a></li>
+    <li><a href="#"> <i class="fa fa-heart"></i> 特别感谢</a></li>
+    <li role="separator" class="divider"></li>
+    <li> <a href="/logout">  <i class="fa fa-sign-out"></i> 退出登录</a></li>
+</ul>
